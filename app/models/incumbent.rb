8509a28c9f8e63253ee535846018d155876d414f
mph372 <mpherron@gmail.com>
2020-06-10 11:53:43 -0700

    Created incumbent view

--- app/models/incumbent.rb
+++ app/models/incumbent.rb
@@ -1,4 +1,9 @@
 class Incumbent < ApplicationRecord
     belongs_to :district
     mount_uploader :image, IncumbentUploader
+
+    def calculated_age
+        now = Time.now.utc.to_date
+        now.year - birth_date.year - ((now.month > birth_date.month || (now.month == birth_date.month && now.day >= birth_date.day)) ? 0 : 1)
+    end
 end
