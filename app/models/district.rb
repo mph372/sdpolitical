c0ab21a3e30e8f2db3d2bf657175d7166d36b1a7
mph372 <mpherron@gmail.com>
2020-06-12 16:51:02 -0700

    Revised districts index page by condensing the table and adding in reg. advantage

--- app/models/district.rb
+++ app/models/district.rb
@@ -2,4 +2,8 @@ class District < ApplicationRecord
   belongs_to :jurisdiction
   has_many :incumbents
   has_many :candidates
+
+  def registration_advantage
+    dem_percent - rep_percent
+  end
 end
