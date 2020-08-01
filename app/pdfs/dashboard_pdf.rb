adb1fc13f085229589cbc6eafeb92b1c28c0a618
mph372 <mpherron@gmail.com>
2020-08-01 13:15:08 -0700

    Attempted to fix bug with pdf generation

--- app/pdfs/dashboard_pdf.rb
+++ app/pdfs/dashboard_pdf.rb
@@ -25,9 +25,8 @@ class DashboardPDF < Prawn::Document
     end
 
     def line_item_rows
-        
         [["Jurisdiction", "District", "Name", "Total Raised", "Total Spent", "Cash-On-Hand", "Net COH", "As of"]] +
         @candidates.map do |candidate|
-            [candidate.district.jurisdiction.name, candidate.district.district_name, candidate.full_name, number_to_currency(candidate.reports.where(:cycle => "2020", candidate_report: true ).sum(:period_receipts)), number_to_currency(candidate.reports.where(:cycle => "2020", candidate_report: true ).sum(:period_disbursements)), number_to_currency(candidate.reports.order('period_end DESC').first.current_coh), number_to_currency(candidate.reports.order('period_end DESC').first.net_coh), candidate.reports.where(candidate_report: true).order(:period_end).last.period_end.strftime("%m/%d/%Y")]
+            [candidate.district.jurisdiction.name, candidate.district.district_name, candidate.full_name, number_to_currency(candidate.reports.where(:cycle => "2020", candidate_report: true ).sum(:period_receipts)), number_to_currency(candidate.reports.where(:cycle => "2020", candidate_report: true ).sum(:period_disbursements)), number_to_currency(candidate.reports.where(:cycle => "2020", candidate_report: true ).order('period_end DESC').first.current_coh), number_to_currency(candidate.reports.order('period_end DESC').first.net_coh), candidate.reports.where(candidate_report: true).order(:period_end).last.period_end.strftime("%m/%d/%Y")]
         end
     end
