2ec275758f41321840a1103ce7161b047bc1f319
mph372 <mpherron@gmail.com>
2020-08-03 15:14:39 -0700

    Added trackable to users and added database schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :dashboardable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable, :trackable
 
   has_many :districts 
   acts_as_follower
