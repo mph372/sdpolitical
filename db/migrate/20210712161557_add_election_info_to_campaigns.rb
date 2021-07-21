36a60e4e900c26ed6fa15c2ec11361d9897917f3
mph372 <mpherron@gmail.com>
2021-07-21 13:46:04 -0700

    Removed false migration

--- db/migrate/20210712161557_add_election_info_to_campaigns.rb
+++
@@ -1,6 +0,0 @@
-class AddElectionInfoToCampaigns < ActiveRecord::Migration[5.2]
-  def change
-    add_column :campaigns, :number_winners, :string
-    add_column :campaigns, :election_type, :string
-  end
-end
