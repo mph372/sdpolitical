9bcb3303e3198fbe7b0d63f4b2c1368bc193d8c8
mph372 <mpherron@gmail.com>
2021-09-10 10:31:30 -0700

    Added honeypot to registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
     prepend_before_action :check_captcha, only: [:create] # Change this to be any actions you want to protect.
-    invisible_captcha only: [:create]
+    before_action :find_bot, only: :create
 
     protected
 
@@ -11,16 +11,8 @@ class RegistrationsController < Devise::RegistrationsController
 
     private
 
-    def check_captcha
-        return if !verify_recaptcha # verify_recaptcha(action: 'signup') for v3
-
-        self.resource = resource_class.new sign_up_params
-        resource.validate # Look for any other validation errors besides reCAPTCHA
-        set_minimum_password_length
-
-        respond_with_navigational(resource) do
-        flash.discard(:recaptcha_error) # We need to discard flash to avoid showing it on the next page reload
-        render :new
-        end
+    def find_bot
+        return unless params[:hp] == '1'
+        head :ok
     end
 end
