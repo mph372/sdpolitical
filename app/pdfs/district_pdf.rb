e345386334981b658f32ad7015ed5c389fbefa2b
mph372 <mpherron@gmail.com>
2021-09-15 10:12:11 -0700

    Attempted bug fix

--- app/pdfs/district_pdf.rb
+++ app/pdfs/district_pdf.rb
@@ -71,7 +71,7 @@ class DistrictPDF < Prawn::Document
     end
 
     def incumbent_atlarge
-        @district.jurisdiction.person.each do |incumbent|
+        @district.jurisdiction.people.each do |person|
             if person.district.at_large_district?
             text "Incumbent: #{person.first_name} #{person.last_name} (#{person.party}) - Term Expires: #{person.district.term_expires}"
             end
