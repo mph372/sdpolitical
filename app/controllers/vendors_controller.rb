7d15273578f4d5eb8a7d55e5b5ff0af64d7b7d99
mph372 <mpherron@gmail.com>
2022-06-04 07:09:16 -0700

    Limit on vendors showing on index

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -4,7 +4,7 @@ class VendorsController < ApplicationController
   # GET /vendors
   # GET /vendors.json
   def index
-    @vendors = Vendor.all
+    @vendors = Vendor.all.limit(500)
   end
 
   # GET /vendors/1
