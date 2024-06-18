a0a9120a0db656ee49788d7ab222e0f959ca1a7f
mph372 <mpherron@gmail.com>
2024-06-18 12:28:20 -0700

    fixed bug with campaigns dispalying on jurisdiction page

--- app/models/jurisdiction.rb
+++ app/models/jurisdiction.rb
@@ -4,11 +4,7 @@ class Jurisdiction < ApplicationRecord
     has_many :people, through: :districts
     has_many :campaigns, through: :districts
     has_many :candidates, through: :campaigns 
-    has_many :reports, through: :people
-    has_many :committees
-    has_many :election_histories, through: :districts 
     has_many :former_offices, through: :districts 
-    belongs_to :registration_history, optional: true
     has_many :statistical_datum
     has_many :registration_snapshots, through: :statistical_datum
     has_many :campaigns, through: :districts
