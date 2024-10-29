5e2b940c248b565063108e2766788799cc4b3e7e
mph372 <mpherron@gmail.com>
2024-10-29 10:41:11 -0700

    fixed production.rb config for email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,14 +24,12 @@ config.action_mailer.default_options = {
 }
 
 config.action_mailer.smtp_settings = {
-  address:              'email-smtp.us-west-1.amazonaws.com',
+  address:              'email-smtp.us-west-1.amazonaws.com', # SES region
   port:                 587,
-  user_name:           Rails.application.credentials.dig(:aws, :ses_smtp_username),
-  password:            Rails.application.credentials.dig(:aws, :ses_smtp_password),
+  user_name:            ENV['SES_SMTP_USERNAME'],
+  password:             ENV['SES_SMTP_PASSWORD'],
   authentication:       :login,
-  enable_starttls_auto: true,
-  tls:                 true,
-  ssl:                 true
+  enable_starttls_auto: true
 }
 
 config.action_mailer.default_url_options = { host: 'theballotbook.com', protocol: 'https' }
